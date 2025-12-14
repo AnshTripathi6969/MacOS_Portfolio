07678101b971dcab3624a164cfe26184d7a0c6c9
Ansh Tripathi <anshtripathi2022@vitbhopal.ac.in>
2025-12-14 13:24:38 +0530

    final changes

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -4,8 +4,9 @@ import { Download } from 'lucide-react';
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
-import "react-pdf/dist/page/AnnotationLayer.css";
-import "react-pdf/dist/page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
